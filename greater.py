a443d8e10d52c3ec332442c7b1ed03fa90e2e074
Saranyavaishalini <vgsvaishalini96@gmail.com>
2017-06-21 14:38:17 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,9 +1,12 @@
-a=inupt("enter the value")
+a=input("enter the value")
 b=input("enter the value")
 c=input("enter the value")
 if a>b and a>c:
-  print(str(a) "is greater")
-elsif b>c:
-  print(str(b) "is greater")
+  print(str(a))
+  print("a is greater")
+elif b>c:
+  print(str(b))
+  print("b is greater")
 else:
-  print(str(c) "is greater")
+  print(str(c))
+  print("c is greater")
