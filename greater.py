404d0a42a2d752ed304303adc6f247fee7993132
Saranyavaishalini <vgsvaishalini96@gmail.com>
2017-06-21 13:48:24 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=inupt("enter the value")
+b=input("enter the value")
+c=input("enter the value")
+if a>b and a>c:
+  print(str(a) "is greater")
+elsif b>c:
+  print(str(b) "is greater")
+else:
+  print(str(c) "is greater")
