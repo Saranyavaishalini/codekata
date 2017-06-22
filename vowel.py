acdc7a721468771318b1fa319abbb4f1cee92a12
Saranyavaishalini <vgsvaishalini96@gmail.com>
2017-06-22 07:33:02 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,5 @@
 l = input("Input a letter of the alphabet: ")
-if l in ('a', 'e', 'i', 'o', 'u'):
+if l in ('a','A', 'e','E', 'i','I', 'o','O', 'u','U'):
 	print("%s is a vowel." % l)
 elif l == 'y':
 	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
