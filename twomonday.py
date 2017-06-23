2fcbf8a0710197e0c86eca5ff37a5b13ac604aeb
Saranyavaishalini <vgsvaishalini96@gmail.com>
2017-06-23 09:28:44 +0530

    Create twomonday.py

---
+++ twomonday.py
@@ -0,0 +1,17 @@
+    day=raw_input("enter the day")
+    if day=="monday":
+    	print("true")
+    elif day=="tuesday":
+    	print("true")
+    elif day=="wednesday":
+    	print("true")
+    elif day=="thursday":
+    	print("true")
+    elif day=="friday":
+    	print("true")
+    elif day=="saturday":
+    	print("true")
+    elif day=="sunday":
+    	print("false")
+    else:
+    	print("enter correct day")
